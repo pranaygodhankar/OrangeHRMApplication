b23de996ba3b547fa8877ec4df411b6f5ed97894
HP <HP@DESKTOP-O76P9HF.domain.name>
2024-07-17 15:47:13 +0530

    BDD frame work is created by Team Leader or Sr test engineer

---
+++ src/test/java/TeamLeaderTutorial/Practice1.java
@@ -0,0 +1,12 @@
+package TeamLeaderTutorial;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("GitHub life cycle is created by Team Leader");
+
+	}
+
+}
