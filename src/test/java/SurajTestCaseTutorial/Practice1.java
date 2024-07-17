3c2027efbdfdcd1e61744d4bb40abd2d8b4d41b1
HP <HP@DESKTOP-O76P9HF.domain.name>
2024-07-17 16:06:57 +0530

    Login Test page create by Suraj Tester

---
+++ src/test/java/SurajTestCaseTutorial/Practice1.java
@@ -0,0 +1,11 @@
+package SurajTestCaseTutorial;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Login test page created by Suraj Automation Tester");
+
+	}
+
+}
